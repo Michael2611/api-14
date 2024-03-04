98aaf61e03a1c0123a19bc2c946d8fbb4960d6b7
Michael <maicolr62@gmail.com>
2024-03-04 01:58:11 -0500

    iddpppp

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -514,7 +514,7 @@ const actualizar_avatar_usuario = async function(req, res){
                 avatar: img.split('\\')[2]
             });
         if(usuario){
-            res.status(200).send({data:img.split('\\')[2]});
+            res.status(200).send({data:img});
         }else{
             res.status(200).send({data:'algo fallo'});
         }
